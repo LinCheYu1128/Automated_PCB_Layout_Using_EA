f0aafa1a9a0466a89af279b4e7d0959584d7486e
danlu008 <danlu008@gmail.com>
2022-02-10 00:38:13 +0800

    modify typesetting at main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,11 @@
+#include "BinaryTree.h"
+#include "ComponentList.h"
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <map>
 #include <ctime>
 using namespace std;
-
-// Class
-#include "BinaryTree.h"
-// #include "Contour.h"
-// #include "TreeNode.h"
-// #include "Component.h"
-#include "ComponentList.h"
-
-// Function
 
 const Component_Path comp_info = {
     "component.csv",                    // component_csvfile
@@ -25,12 +18,9 @@ int main () {
     
     ComponentList* component_list = new ComponentList(comp_info);
     component_list->printData("T501");
-
-    // map<string, Point> pin_position = {{"A", {1, 0}}, {"B", {2, 2}}, {"C", {3, 3}}, {"D", {4, 4}}, {"E", {4, 4}}};
-    // ComponentProperty* comp_prop = new ComponentProperty();
-    // comp_prop->setAllPinPosition(pin_position);
     
     BinaryTree* tree = new BinaryTree(component_list);
     tree->printBinaryTree();
+    
     return 0;
 }
