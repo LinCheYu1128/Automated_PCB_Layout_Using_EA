7add5e39bbe2593bf64f3d0fee8de12263fedb97
danlu008 <danlu008@gmail.com>
2022-02-10 16:11:44 +0800

    add comment at maine.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 const Component_Path comp_info = {
-    "component.csv",                    // component_csvfile
+    "component.csv",                     // component_csvfile
     "..\\resources\\",                   // component_relativePath
     "..\\resources\\pin position\\"      // pinPosition_relativePath
 };
@@ -14,10 +14,12 @@ int main () {
     srand((unsigned)time(0));
     
     ComponentList* component_list = new ComponentList(comp_info);
-    component_list->printData("T501");
+    // component_list->printData("T501");   // test log component data
     
     BinaryTree* tree = new BinaryTree(component_list);
-    tree->setSingleSide();
+    tree->setSingleSide();  // single side
+    // tree->setDoubleSide();  // double side
+    
     tree->printBinaryTree();
 
     return 0;
